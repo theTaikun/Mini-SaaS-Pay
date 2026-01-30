28d1f679e80f2d93913dcd6420d65f67fd9ec707
Isaac Phillips <43371347+theTaikun@users.noreply.github.com>
2026-01-30 10:17:45 +0100

    Finished implementing login, allow logout

--- routers/users.py
+++ routers/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, session
+from flask import Blueprint, jsonify, redirect, request, session, url_for
 from sqlalchemy import select
 from sqlalchemy.dialects.sqlite import insert
 
@@ -36,3 +36,29 @@ def add_user():
         conn.close()
     session["id"] = response.id
     return dict(response), 201
+
+
+@user_bp.route("/login/", methods=["POST"])
+def login_submitted():
+    data = request.json
+    stmt = (
+        select(User)
+        .where(User.email==data["username"], User.password==data["password"])
+    )
+    with SessionLocal() as conn:
+        response = conn.execute(stmt)
+        result= response.scalar_one_or_none()
+        conn.close()
+    if result:
+        session["id"] = result.id
+        return jsonify({"status":"Success"}), 200
+    else:
+        return jsonify({"status":"Invalid Login"}), 401
+
+
+@user_bp.route("/logout/")
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template, session
+from flask import Flask, redirect, render_template, session, url_for
 import os
 
 from routers.stripe import stripe_bp
@@ -25,19 +25,32 @@ def home():
 @app.route("/register/")
 def register():
     if "id" in session:
-        # TODO: Redirect elsewhere
-        print(session)
+        return redirect(url_for('dashboard'))
     return render_template(
        'register.html',
        STRIPE_PUBLISHABLE_KEY=STRIPE_PUBLISHABLE_KEY,
        price_id=os.getenv("STRIPE_RECURRING_PRICE_ID"),
        )
 
-@app.route("/login/")
-def login():
+@app.route("/login/", methods=["GET"])
+def login_page():
+    if "id" in session:
+        return redirect(url_for('dashboard'))
     return render_template(
        'login.html',
     )
+
+@app.route("/dashboard/")
+def dashboard():
+    if "id" in session:
+        s = dict(session)
+        return render_template(
+            'dashboard.html',
+        data=s
+    )
+    else:
+        return redirect(url_for('login'))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
